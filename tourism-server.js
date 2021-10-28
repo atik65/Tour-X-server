8ca6b8b30b55a63854d990348d39a0e09237a4f2
Atik Hasan <atikhasan801@gmail.com>
2021-10-29 01:25:48 +0600

    second commit

--- tourism-server.js
+++ tourism-server.js
@@ -14,9 +14,8 @@ const port = process.env.PORT || 5000;
 
 // connection to mongodb
 
-// const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS}@cluster0.tx5hg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS}@cluster0.tx5hg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://tourism:OuM7iIgBMf2JT4Vk@cluster0.tx5hg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
